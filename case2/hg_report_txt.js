45dff10f0753e87fb48e8dde48814e85a9c41053
Elijah Collins <ecollins152@west-mec.org>
2020-02-04 15:28:43 -0700

    case 2 work

--- case2/hg_report_txt.js
+++ case2/hg_report_txt.js
@@ -5,9 +5,11 @@
    Tutorial 10
    Case Problem 2
 
-   Author: 
-   Date:   
+   Author: Elijah Collins
+   Date:   2/4/20
    
    Filename: hg_report.js
 	
 */
+
+var gameReport = "<h1>title></h1><h2>By: " + itemManufacturer + "</h2><img src='hg_" + itemID + ".png' alt='" + itemID + "' id='gameImg' /> <table><tr><th>Product ID</th><td>" + itemID + "</td><tr>"
